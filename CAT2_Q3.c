2d72821075fa54480eb868657dfafe17db081b34
mkuualexis-a11y <mkuualexis@gmail.com>
2025-11-19 09:45:08 +0300

    Refactor user prompts and error handling in CAT2_Q3.c
    Updated prompts and error messages for clarity.

--- CAT2_Q3.c
+++ CAT2_Q3.c
@@ -13,23 +13,26 @@ FILE *f = fopen("data.txt", "w+");
     int n = 10, i, x, sum = 0;
     float avg;
 
-    if (!f) return printf("File error\n"), 1;
-
-    printf("Enter 10 integers:\n");
+    if (!f){
+		return printf("Ooops!we seem to have a file error\n"), 1;
+	}
+    printf("Please enter 10 integers,one at a time:\n");
     for (i = 0; i < n; i++) {
         scanf("%d", &x);
-        fprintf(f, "%d\n", x);
+        fprintf(f, "%d \n", x);
         sum += x;
     }
-    avg = (float)sum / n;
+    avg = (float)sum /n;
 
     fprintf(f, "Sum=%d\nAverage=%.2f\n", sum, avg);
 
     rewind(f);
-    printf("\n--- data.txt contents ---\n");
-    while ((x = fgetc(f)) != EOF) putchar(x);
-
+    printf("\n---Here is what we have in data.txt contents ---\n");
+    while ((x = fgetc(f)) != EOF){
+		putchar(x);
+	}
     fclose(f);
 
 	return 0;
+
 }
